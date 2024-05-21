21d9a5b2b94a4908af15ad8a4847f94f14718f0b
shantelle04 <shantelle.nkatha@strathmore.edu>
2024-05-21 14:01:22 +0300

    Added a functionality

---
+++ arrays.php
@@ -0,0 +1,68 @@
+<h1>Arrays</h1>
+<?php
+
+$color = array("black","green");
+
+print_r($color);
+
+?>
+<pre>
+    <?php print_r($color); ?>
+</pre>
+
+
+<h1>Associative Arrays</h1>
+<?php
+
+
+$user_emails= array(
+    "Alex"=>"aokama@yahoo.com",
+    "peter"=>"pokama@yahoo.com",
+    "Amani"=>"amani@yahoo.com"
+);
+?>
+<pre>
+    <?php print_r($user_emails); ?>
+</pre>
+
+<h1>Multidimensional Arrays</h1>
+<?php
+$user_details = [
+    "Director"=> [
+        "Fullname"=>" Alex Okama",
+        "Email"=> "Okama@yahoo.com",
+        "Address"=> "mada",
+        "phone"=>[
+            "Mobile"=> "0790727208",
+            "Cell"=> "0720819692",
+            "Work"=>"0722828803"
+        ],
+    ],
+    "Manager" =>[
+        "fullname" => "Juliette Okama",
+        "email" => "jokama@yahoo.com",
+        "address" => "Mada",
+        "phone" => [
+               "mobile " => "+254737882823",
+               "cell" => "+25473756823",
+               "work" => "+254737882823",
+        ],
+    ],
+
+    "Secretary"=>[
+        "fullname" => "Juliette Okama",
+        "email" => "jokama@yahoo.com",
+        "address" => "Mada",
+        "phone" => [
+               "mobile " => "+254737882823",
+               "cell" => "+25473756823",
+               "work" => "+254737882823",
+        ],
+    ],
+];
+
+?>
+
+<pre>
+    <?php print_r($user_details); ?>
+</pre>
---
+++ loops.php
@@ -0,0 +1,37 @@
+<h1>Loops</h1>
+
+<?php
+//whole loop 
+
+$max= 0;
+while($max< 10){
+    print $max. "br";
+    $max++;
+}
+?>
+<h4>do while loop</h4>
+<?php
+
+//do while loop
+$x=4;
+do{
+    print $x . "<br>";
+    $x++;
+}while($x < 7)
+
+?>
+<h4>for loop</h4>
+<?php
+//for loop
+for($r=3; $r<12; $r++){
+    print $r . "br";
+}
+?> 
+
+<h4>for loop</h4>
+<?php
+//for loop
+for($n=14; $n<24; $n+2){
+    print $r . "br";
+}
+?>
