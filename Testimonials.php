43e1de9e6e2fd5c0858ac21779e47ef2081e13f1
shantelle04 <shantelle.nkatha@strathmore.edu>
2024-06-24 13:12:54 +0300

    Added a functionality

--- Testimonials.php
+++ Testimonials.php
@@ -69,7 +69,8 @@ body{
             color: rgb(109, 107, 107);
             text-decoration: wavy;
         }
-    </style>
+    </style><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <!-- Navigation -->
