2766ee9ae0f49ceb8bf1c32d843bbdac6fcc7c82
shantelle04 <shantelle.nkatha@strathmore.edu>
2024-07-01 13:29:28 +0300

    moved to php

--- Templates/nav.php
+++ Templates/nav.php
@@ -1,12 +1,12 @@
 <!-- Navigation -->
 <nav>
         <ul>
-            <li><a href="index.html">HomePage</a></li>
-            <li><a href="About Us.html">About Us</a></li>
-            <li><a href="season.html">4 the szn</a></li>
-            <li><a href="category.html">Category</a></li>
-            <li><a href="Testimonials.html">Testimonials</a></li>
-            <li><a href="Contact Us.html">Contact Us</a></li>
-            <li><a href="footer.html">Footer</a></li>
+        <li><a href="index.php">HomePage</a></li>
+            <li><a href="About Us.php">About Us</a></li>
+            <li><a href="season.php">4 the szn</a></li>
+            <li><a href="category.php">Category</a></li>
+            <li><a href="Testimonials.php">Testimonials</a></li>
+            <li><a href="Contact Us.php">Contact Us</a></li>
+            <li><a href="footer.php">Footer</a></li>
         </ul>
     </nav>
