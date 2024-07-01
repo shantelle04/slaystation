276d28e332eec11d74638e2b030184c94c1e9b60
shantelle04 <shantelle.nkatha@strathmore.edu>
2024-07-01 13:28:58 +0300

    Added a functionality

--- Testimonials.php
+++ Testimonials.php
@@ -74,17 +74,7 @@ body{
 </head>
 <body>
     <!-- Navigation -->
-    <nav>
-        <ul>
-            <li><a href="index.html">HomePage</a></li>
-            <li><a href="About Us.html">About Us</a></li>
-            <li><a href="season.html">4 the szn</a></li>
-            <li><a href="category.html">Category</a></li>
-            <li><a href="Testimonials.html">Testimonials</a></li>
-            <li><a href="Contact Us.html">Contact Us</a></li>
-            <li><a href="footer.html">Footer</a></li>
-        </ul>
-    </nav>
+   
     </header>
     <main>
         <h2>Get in Touch</h2>
