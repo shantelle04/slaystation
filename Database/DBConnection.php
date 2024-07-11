696ae1b988d6669619201cb2a3f19d31bc8335a9
shantelle04 <shantelle.nkatha@strathmore.edu>
2024-07-11 11:09:26 +0300

    modiieed

--- Database/DBConnection.php
+++ Database/DBConnection.php
@@ -5,14 +5,14 @@ $password = "";
 $dbname = "DB";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$DBConnection = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
-if (!$conn) {
+if (!$DBConnection) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
 // ... Your PHP code to process and store comments ...
 
-mysqli_close($conn);
+mysqli_close($DBConnection);
 ?>
